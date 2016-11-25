4cadbc345eca40fce1e11a065aa7d96a91fce4a9
Alan Choi <alanchoi08@gmail.com>
2016-11-24 20:39:21 -0800

    remove config

--- config/firebase.config.js
+++ config/firebase.config.js
@@ -1,5 +1,9 @@
 const fireConfig = {
-
+  apiKey: "<API_KEY>",
+    authDomain: "<PROJECT_ID>.firebaseapp.com",
+    databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+    storageBucket: "<BUCKET>.appspot.com",
+    messagingSenderId: "<SENDER_ID>",
 };
 
 export default fireConfig;
